85d93202a714813e979a3e0a565946b3b89fa237
Takanori Ugai <ugai@fujitsu.com>
2023-01-22 23:29:23 +0900

    fix an error

--- data/prepare.sh
+++ data/prepare.sh
@@ -3,4 +3,4 @@ wget https://github.com/KnowledgeGraphJapan/KGRC-RDF/archive/refs/heads/ikgrc202
 unzip ikgrc2023.zip
 mv KGRC-RDF-ikgrc2023/* .
 rm ikgrc2023.zip
-rmdir KGRC-RDF-ikgrc2023
+rm -r KGRC-RDF-ikgrc2023
